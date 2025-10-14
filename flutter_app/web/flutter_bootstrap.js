5b1f76b02230d11d098483cd2454b4cbccad6242
Andy Horn <andyjhorn@gmail.com>
2025-10-14 14:46:09 -0700

    remove comments from bootstrap template file

--- flutter_app/web/flutter_bootstrap.js
+++ flutter_app/web/flutter_bootstrap.js
@@ -1,12 +1,3 @@
-// This file is added directly to the Flutter web project and will 
-// be included in the final build output.
-//
-// It provides a template for the bootstrap JavaScript code that initializes
-// the Flutter web application.
-//
-// The placeholders {{flutter_js}} and {{flutter_build_config}} will be
-// replaced with the actual content during the build process.
-
 {{flutter_js}}
 
 {{flutter_build_config}}
@@ -15,4 +6,4 @@ _flutter.loader.load({
     config: {
         hostElement: document.getElementById('flutter_container'),
     }
-})
+});
