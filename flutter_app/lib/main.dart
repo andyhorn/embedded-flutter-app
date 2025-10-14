93dd8077c7a32d5458f281d3054ae1eb9dcbb56f
Andy Horn <andyjhorn@gmail.com>
2025-10-14 16:08:34 -0700

    add some comments

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/multi_view_app.dart';
 
 void main() {
+  // Run this MultiViewApp widget, which is essentially global.
+  // Any time a view is added via the JavaScript API, it will call the
+  // viewBuilder function to create a new instance of MainApp.
   runWidget(MultiViewApp(viewBuilder: (context) => const MainApp()));
 }
 
@@ -10,6 +13,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // Get the unique view ID for this instance of MainApp.
     final id = View.of(context).viewId;
 
     return MaterialApp(
