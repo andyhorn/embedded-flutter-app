d5eb688eff59bdafc7eca48d7c7248365b12993c
Andy Horn <andyjhorn@gmail.com>
2025-10-14 15:29:27 -0700

    restore the Vue app

--- flutter_app/web/flutter_bootstrap.js
+++ flutter_app/web/flutter_bootstrap.js
@@ -4,28 +4,11 @@
 
 _flutter.loader.load({
     onEntrypointLoaded: async (engineInitializer) => {
-        console.log('initializing engine')
-        let engine = await engineInitializer.initializeEngine({
+        const engine = await engineInitializer.initializeEngine({
             multiViewEnabled: true,
         });
 
-        console.log('running app')
-        let app = await engine.runApp().then((app) => {
-            console.log('app running')
-
-            let element = document.getElementById('flutter_container');
-
-            if (!element) {
-                throw 'Could not find element with id "flutter_container"';
-            }
-
-            let view = app.addView({
-                hostElement: element,
-            });
-
-            console.log('view added to page');
-            return app;
-        });
-
+        // Run the app and store a reference in the flutterApp variable.
+        const flutterApp = await engine.runApp();
     },
 });
--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -40,28 +40,11 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
     onEntrypointLoaded: async (engineInitializer) => {
-        console.log('initializing engine')
-        let engine = await engineInitializer.initializeEngine({
+        const engine = await engineInitializer.initializeEngine({
             multiViewEnabled: true,
         });
 
-        console.log('running app')
-        let app = await engine.runApp().then((app) => {
-            console.log('app running')
-
-            let element = document.getElementById('flutter_container');
-
-            if (!element) {
-                throw 'Could not find element with id "flutter_container"';
-            }
-
-            let view = app.addView({
-                hostElement: element,
-            });
-
-            console.log('view added to page');
-            return app;
-        });
-
+        // Run the app and store a reference in the flutterApp variable.
+        const flutterApp = await engine.runApp();
     },
 });
